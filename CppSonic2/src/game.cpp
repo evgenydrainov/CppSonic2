0c4664a2680010734f59e3a87ae22bdb77676a92
evgenydrainov <79319450+evgenydrainov@users.noreply.github.com>
2024-11-01 15:57:25 +0300

    start doing gameplay

--- CppSonic2/src/game.cpp
+++ CppSonic2/src/game.cpp
@@ -20,6 +20,20 @@ void Game::init() {
 	font = load_bmfont_file("fonts/ms_gothic.fnt", "fonts/ms_gothic_0.png");
 
 	load_level("levels/GHZ_Act1");
+
+	// @Leak
+	texture_crouch   = load_texture_from_file("textures/crouch.png");
+	texture_idle     = load_texture_from_file("textures/idle.png");
+	texture_look_up  = load_texture_from_file("textures/look_up.png");
+	texture_peelout  = load_texture_from_file("textures/peelout.png");
+	texture_roll     = load_texture_from_file("textures/roll.png");
+	texture_run      = load_texture_from_file("textures/run.png");
+	texture_skid     = load_texture_from_file("textures/skid.png");
+	texture_spindash = load_texture_from_file("textures/spindash.png");
+	texture_walk     = load_texture_from_file("textures/walk.png");
+
+	player.pos.x = window.game_width  / 2.0f;
+	player.pos.y = window.game_height / 2.0f;
 }
 
 void Game::deinit() {
@@ -33,7 +47,5 @@ void Game::update(float delta) {
 void Game::draw(float delta) {
 	renderer.proj_mat = glm::ortho<float>(0, window.game_width, window.game_height, 0);
 
-	draw_texture(tileset_texture);
-
-	draw_text(font, "Hello, World!", {10, 10});
+	draw_texture_centered(texture_idle, player.pos);
 }
--- CppSonic2/src/game.h
+++ CppSonic2/src/game.h
@@ -5,10 +5,32 @@
 #include "font.h"
 #include "texture.h"
 
+struct Player {
+	vec2 pos;
+	vec2 speed;
+
+	float ground_speed;
+	float ground_angle;
+
+	vec2 radius = {9, 19};
+};
+
 struct Game {
+	Player player;
+
 	Font font;
 
 	Texture tileset_texture;
+
+	Texture texture_crouch;
+	Texture texture_idle;
+	Texture texture_look_up;
+	Texture texture_peelout;
+	Texture texture_roll;
+	Texture texture_run;
+	Texture texture_skid;
+	Texture texture_spindash;
+	Texture texture_walk;
 
 	void init();
 	void deinit();
